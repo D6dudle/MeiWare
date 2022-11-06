9c52a38e475f01e1bb4813a9c614706dfebd55be
DJordao <diogojordao.2504@gmail.com>
2022-11-06 10:48:04 +0000

    Repositórios terminados. Falta testar.

--- backend/course-management/src/main/java/meiware/coursemanagement/Repositories/ColaboradorRepository.java
+++
@@ -1,5 +0,0 @@
-package meiware.coursemanagement.Repositories;
-
-
-public class ColaboradorRepository implements IColaboradorRepository{
-}
--- backend/course-management/src/main/java/meiware/coursemanagement/Services/teste.java
+++ backend/course-management/src/main/java/meiware/coursemanagement/Services/teste.java
@@ -1,4 +1,15 @@
 package meiware.coursemanagement.Services;
 
+import meiware.coursemanagement.Repositories.IPedidoFormacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class teste {
+    @Autowired
+    private IPedidoFormacaoRepository pedidoFormacaoRepository;
+
+    public void Teste() {
+        pedidoFormacaoRepository.findAll();
+    }
 }
