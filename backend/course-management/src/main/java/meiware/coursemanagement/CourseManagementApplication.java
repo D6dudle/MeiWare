4a6a4ca8606f182057011f49379c6a6bb1b430d8
Pedro Marques <ptiagosm@gmail.com>
2022-11-08 18:15:10 +0000

    JPA entities testing

--- backend/course-management/src/main/java/meiware/coursemanagement/CourseManagementApplication.java
+++ backend/course-management/src/main/java/meiware/coursemanagement/CourseManagementApplication.java
@@ -9,6 +9,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class CourseManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseManagementApplication.class, args);
+		//testJPA();
+	}
+
+	static void testJPA(){
+		JPAtest jpAtest = new JPAtest();
+		jpAtest.demo();
 	}
 
 }
