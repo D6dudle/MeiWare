3794351b92d83e9266c02dec1db7dd551e0b5ded
Francisco <jf.bugalho@hotmail.com>
2022-11-19 17:39:09 +0000

    Cleanup

--- Frontend/src/pages/GerirPedidos.jsx
+++ Frontend/src/pages/GerirPedidos.jsx
@@ -13,36 +13,6 @@ export default function GerirPedidos() {
   const [activeFilter, setActiveFilter] = useState(null);
   const [filter, setFilter] = useState(null);
 
-  const dataCard = [
-    {
-      username: "Jane Doe",
-      nomeformacao: "Introdução a react",
-      dataFormacao: "18/11/2022 14:00",
-      justificacaoFormacao:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      idCurso: "G-C-765",
-      tipoFormacao: "CURSO",
-    },
-    {
-      username: "Jane Doe",
-      nomeformacao: "Introdução a react",
-      dataFormacao: "18/11/2022 14:00",
-      justificacaoFormacao:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      idCurso: "G-C-765",
-      tipoFormacao: "CURSO",
-    },
-    {
-      username: "Jane Doe",
-      nomeformacao: "Introdução a react",
-      dataFormacao: "18/11/2022 14:00",
-      justificacaoFormacao:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      idCurso: "G-C-765",
-      tipoFormacao: "CURSO",
-    },
-  ];
-
   return (
     <div className="ml-8 mr-8">
       <h1 className="text-white font-bold text-3xl mt-8">Gerir formações</h1>
