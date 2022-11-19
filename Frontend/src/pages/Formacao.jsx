51774960df9f0badcfa4311cfd7a1fe5e5b97ba1
Jose Miguel Silva Gomes <jose.miguel.gomes@hotmail.com>
2022-11-19 17:50:31 +0000

    Ecrã formação - started

--- Frontend/src/pages/Formacao.jsx
+++ Frontend/src/pages/Formacao.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
 import { Outlet } from "react-router-dom";
-
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function Formacao() {
+  const percentage = 66;
+  /* https://codesandbox.io/s/vymm4oln6y?file=%2FAnimatedProgressProvider.js */
+
   return (
     <div>
-      <p>Formação</p>
-      
+      <div className="w-36 font-extrabold text-2xl">
+        <CircularProgressbar
+          value={percentage}
+          text={`${percentage}%`}
+          styles={buildStyles({
+            textColor: "white",
+            pathColor: "#ECC039",
+            trailColor: "#FFEFA1",
+            textSize: "28px"
+          })}
+        />
+      </div>
     </div>
-  )
+  );
 }
