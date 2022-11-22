39abb856f7ef4de1054e45c8e10b52d002efb429
Jose Miguel Silva Gomes <jose.miguel.gomes@hotmail.com>
2022-11-22 12:49:35 +0000

    Ecrã Controlo Budget

--- Frontend/src/components/StatsCard.jsx
+++ Frontend/src/components/StatsCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ChevronsUp } from "react-feather";
 import { LineChart } from "../components/LineChart";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 
 export const StatsCard = ({titulo, valor, valorAumento}) => {
@@ -15,11 +17,38 @@ export const StatsCard = ({titulo, valor, valorAumento}) => {
             + {valorAumento} <span className="text-white"> (mês passado)</span>{" "}
           </span>
         </div>
-        {/* Gráfico de linhas !!*/}
+        
       </div>
+      {/* Gráfico de linhas !!*/}
       <div className="w-40">
         <LineChart />
       </div>
     </div>
   );
 };
+
+export const StatsLargeCard = ({titulo, montante, descricao, percentagem}) => {
+  return(
+    <div className="flex flex-col gap-8 justify-center items-center md:flex-row md:justify-start md:gap-20 max-w-[600px] border-[3px] border-primary rounded pl-3 pb-4 pt-4 pr-3 ">
+          <div className="flex flex-col gap-4">
+            <p className="text-base text-gray3 font-bold">{titulo}</p>
+            <p className=" text-3xl font-bold text-white ">{montante}</p>
+            <p className="text-[9px] font-normal max-w-[160px]">
+             {descricao}
+            </p>
+          </div>
+          <div className="w-28 font-extrabold text-2xl">
+            <CircularProgressbar
+              value={percentagem}
+              text={`${percentagem}%`}
+              styles={buildStyles({
+                textColor: "white",
+                pathColor: "#ECC039",
+                trailColor: "#FFEFA1",
+                textSize: "28px",
+              })}
+            />
+          </div>
+        </div>
+  )
+}
--- Frontend/src/pages/Formacao.jsx
+++ Frontend/src/pages/Formacao.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
-import { StatsCard } from "../components/StatsCard";
+import { StatsCard, StatsLargeCard } from "../components/StatsCard";
 
 
 
 
 export default function Formacao() {
-  const percentage = 55;
   /* https://codesandbox.io/s/vymm4oln6y?file=%2FAnimatedProgressProvider.js */
 
   return (
@@ -15,28 +12,12 @@ export default function Formacao() {
       <h1 className="text-2xl font-bold">Bem vinda utilizador!</h1>
       <div className="pt-12 ">
         <h1 className="text-4xl font-bold pb-4">Dashboard Overview</h1>
-        <div className="flex flex-col gap-8 justify-center md:flex-row md:justify-start md:gap-20 max-w-[600px] border-[3px] border-primary rounded pl-3 pb-4 pt-4 pr-3 ">
-          <div className="flex flex-col gap-4">
-            <p className="text-base text-gray3 font-bold">Orçamento restante</p>
-            <p className=" text-3xl font-bold text-white ">€ 542,65</p>
-            <p className="text-[9px] font-normal max-w-[140px]">
-              Valor calculado após a validação de cada formação pretendida
-            </p>
-          </div>
-          <div className="w-28 font-extrabold text-2xl">
-            <CircularProgressbar
-              value={percentage}
-              text={`${percentage}%`}
-              styles={buildStyles({
-                textColor: "white",
-                pathColor: "#ECC039",
-                trailColor: "#FFEFA1",
-                textSize: "28px",
-              })}
-            />
-          </div>
-        </div>
-
+        <StatsLargeCard
+          titulo='Orçamento restante'
+          montante='€ 542,65'
+          descricao='Valor calculado após a validação de cada formação pretendida'
+          percentagem={55}
+        />
         <div className="flex flex-col lg:flex-row lg:gap-8">
         {/** @MakeComponent : fazer desta DIV um componente de estatisticas*/ }
         <StatsCard
