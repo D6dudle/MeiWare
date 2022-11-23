fd63696130bdc86a511133b01e82eabf93f54019
DJordao <diogojordao.2504@gmail.com>
2022-11-23 18:16:28 +0000

    Fix no run dos testes e implementação de parte do serviço de publicações.

--- backend/course-management/src/main/java/meiware/coursemanagement/CourseManagementApplication.java
+++ backend/course-management/src/main/java/meiware/coursemanagement/CourseManagementApplication.java
@@ -1,5 +1,7 @@
 package meiware.coursemanagement;
 
+import meiware.coursemanagement.Repositories.MongoDB.IAnexoRepository;
+import meiware.coursemanagement.Repositories.MongoDB.IPublicacaoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,18 +9,20 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication(scanBasePackages = "meiware.coursemanagement")
-@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "meiware.coursemanagement.Repositories.MongoDB.*"))
-@EnableMongoRepositories(basePackages = "meiware.coursemanagement.Repositories")
+@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = {IPublicacaoRepository.class, IAnexoRepository.class}))
+@EnableMongoRepositories(basePackages = "meiware.coursemanagement.Repositories.MongoDB")
 public class CourseManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseManagementApplication.class, args);
 		testJPA();
 	}
 
-	static void testJPA(){
+	@PostConstruct
+	static void testJPA() {
 		JPAtest jpAtest = new JPAtest();
-		jpAtest.demo();
 	}
 
 }
--- backend/course-management/src/main/java/meiware/coursemanagement/Services/MongoDB/IPublicacaoService.java
+++ backend/course-management/src/main/java/meiware/coursemanagement/Services/MongoDB/IPublicacaoService.java
@@ -4,7 +4,12 @@ import meiware.coursemanagement.Entities.MongoDB.Publicacao;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface IPublicacaoService {
-
+    List<Publicacao> getPublicacoes();
+    Publicacao getPublicacaoById(String id);
+    String createPublicacao(Publicacao newPublicacao, List<MultipartFile> files) throws IOException;
+    void updatePublicacao(Publicacao updatedPublicacao);
+    void removePublicacao(Publicacao publicacao);
 }
