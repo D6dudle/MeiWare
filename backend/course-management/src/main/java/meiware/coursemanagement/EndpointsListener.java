eb4e9384bdac71aae624ab30cc95586796c239ea
Henrique Teixeira Fonseca <henri.teix@gmail.com>
2022-12-12 12:35:27 +0000

    Continuação da ligação Cliente-Servidor: pedido axios para controladores

---
+++ backend/course-management/src/main/java/meiware/coursemanagement/EndpointsListener.java
@@ -0,0 +1,19 @@
+package meiware.coursemanagement;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Component
+public class EndpointsListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext applicationContext = event.getApplicationContext();
+        applicationContext.getBean(RequestMappingHandlerMapping.class).getHandlerMethods()
+                .forEach((key, value) -> System.out.println("value: " + value + " key: " + key));
+    }
+}
--- backend/course-management/src/main/java/meiware/coursemanagement/Security/JWT/AuthTokenFilter.java
+++ backend/course-management/src/main/java/meiware/coursemanagement/Security/JWT/AuthTokenFilter.java
@@ -38,7 +38,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
                         userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                logger.error(authentication.toString());
+
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception e) {
