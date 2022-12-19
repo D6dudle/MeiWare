90e3ca5bd9af084d91abb2beb54bdc2891e4b7cb
Pedro Marques <ptiagosm@gmail.com>
2022-12-19 19:38:37 +0000

    Code refactor

--- backend/course-management/src/test/java/meiware/coursemanagement/ServicesMock/JPA/BudgetServiceMock.java
+++ backend/course-management/src/test/java/meiware/coursemanagement/ServicesMock/JPA/BudgetServiceMock.java
@@ -10,8 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
