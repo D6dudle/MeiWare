8ecc6e99722bc2a66d818f688c4ffc10c0b1d09a
Francisco <jf.bugalho@hotmail.com>
2023-01-10 13:05:41 +0000

    Search bar and dropdown fixed
    Done search bar and dropdown in the screens:
      - PesquisarForum;
      - AprovarPublicacao

--- Frontend/src/pages/AprovarPublicacao.jsx
+++ Frontend/src/pages/AprovarPublicacao.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { DollarSign, Search } from "react-feather";
 import TextInput from "../components/TextInput";
 import DropzoneFiles from "../components/Dropzone";
-
+import users from "../constants/usersAux.json";
 import ForumTopic from "../components/ForumTopic";
 import { EmptyState } from "../components/EmptyState";
 
@@ -16,6 +16,7 @@ export default function AprovarPublicacao() {
       descricao:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget lobortis lectus, non sodales purus. Duis eget ex congue, mattis nulla vel, suscipit velit. Cras sollicitudin lectus ut nibh sollicitudin, eu sodales ligula bibendum. Integer elementum congue ultrices. Curabitur justo nulla, scelerisque id pellentesque nec, placerat vel urna. Duis condimentum lacinia auctor. Morbi sed nisl non magna congue convallis vitae sed enim.",
       cursoId: "G-C-765",
+      tags: ["React", "Java"],
     },
     {
       username: "Nelso Gervásio",
@@ -25,12 +26,11 @@ export default function AprovarPublicacao() {
       descricao:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget lobortis lectus, non sodales purus. Duis eget ex congue, mattis nulla vel, suscipit velit. Cras sollicitudin lectus ut nibh sollicitudin, eu sodales ligula bibendum. Integer elementum congue ultrices. Curabitur justo nulla, scelerisque id pellentesque nec, placerat vel urna. Duis condimentum lacinia auctor. Morbi sed nisl non magna congue convallis vitae sed enim.",
       cursoId: "G-C-777",
+      tags: ["React", "Java"],
     },
   ];
 
-  const [publicacoes, setPublicacoes] = useState(pubList);
-
-  const tagList = [
+  const tags = [
     { label: "React", value: "React" },
     { label: "Angular", value: "Angular" },
     { label: "C++", value: "C++" },
@@ -39,39 +39,73 @@ export default function AprovarPublicacao() {
     { label: "Javascript", value: "Javascript" },
   ];
 
-  const users = [
-    { label: "Shark", value: "Shark" },
-    { label: "Dolphin", value: "Dolphin" },
-    { label: "Whale", value: "Whale" },
-    { label: "Octopus", value: "Octopus" },
-    { label: "Crab", value: "Crab" },
-    { label: "Lobster", value: "Lobster" },
-  ];
-
-  const filterTags = (inputValue) => {
-    return tagList.filter((i) =>
-      i.label.toLowerCase().includes(inputValue.toLowerCase())
-    );
-  };
-
-  const filterColaborador = (inputValue) => {
-    return users.filter((i) =>
-      i.label.toLowerCase().includes(inputValue.toLowerCase())
-    );
-  };
-
+  const [publicacoes, setPublicacoes] = useState(pubList);
+  const [filteredList, setFilteredList] = useState(publicacoes);
+  const [search, setSearch] = useState();
+  const [colabList, setColabList] = useState(users);
+  const [tagList, setTags] = useState(tags);
   const [values, setValues] = useState([]);
+  const [tagValues, setTagValues] = useState([]);
+
+  useEffect(() => {
+    var list = publicacoes;
+    if (search && search !== "") {
+      list = list.filter((item) =>
+        item.nomeformacao.toLowerCase().includes(search.toLowerCase())
+      );
+      if (values.length > 0) {
+        for (let i = 0; i < values[1].length; i++) {
+          list = list.filter((item) => item.username == values[1][i].label);
+        }
+      }
+      if (tagValues.length > 0) {
+        for (let i = 0; i < tagValues[1].length; i++) {
+          list = list.filter((item) => item.tags == tagValues[1][i].label);
+        }
+      }
+    } else {
+      if (values.length > 0) {
+        for (let i = 0; i < values[1].length; i++) {
+          list = list.filter((item) => item.username == values[1][i].label);
+        }
+      }
+      if (tagValues.length > 0) {
+        for (let i = 0; i < tagValues[1].length; i++) {
+          list = list.filter((item) => item.tags == tagValues[1][i].label);
+        }
+      }
+    }
+
+    var tempData = [...list];
+    setFilteredList(tempData);
+  }, [search, values, tagValues, publicacoes]);
 
   const handleType = (index, event) => {
-    let data = [...values];
-    data[index] = event.target.value;
-    setValues(data);
+    setSearch(event.target.value);
   };
 
   const handleDropdown = (index, opt) => {
     let data = [...values];
     data[index] = opt;
     setValues(data);
+  };
+
+  const handleDropdownTags = (index, opt) => {
+    let data = [...tagValues];
+    data[index] = opt;
+    setTagValues(data);
+  };
+
+  const filterColab = (inputValue) => {
+    return colabList.filter((i) =>
+      i.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  };
+
+  const filterTags = (inputValue) => {
+    return tagList.filter((i) =>
+      i.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
   };
 
   const handleFormSubmit = (e) => {
@@ -94,42 +128,55 @@ export default function AprovarPublicacao() {
       <div className="sticky top-5">
         <h1 className=" text-white font-bold text-3xl">Aprovar publicação</h1>
         <div className="pt-4">
-          <div className="w-full pb-2">
-            <TextInput
-              index={0}
-              type="searchbar"
-              noTitle={true}
-              callback={handleType}
-              value={values[0]}
-            />
-          </div>
+          <TextInput
+            index={1}
+            name={"pesquisa..."}
+            type={"searchbar"}
+            style={"w-full"}
+            showTitle={false}
+            callback={handleType}
+            value={search}
+          />
 
-          <div className="flex flex-row w-full">
-            <div className="w-full pr-2">
-              <TextInput
-                index={1}
-                name={"tags"}
-                type="dropsearch"
-                list={tagList}
-                multi={true}
-                value={values[1]}
-                placeholder="tags..."
-                callback={handleDropdown}
-                searchCall={filterTags}
-              />
-            </div>
-            <div className="w-full pl-2">
-              <TextInput
-                index={2}
-                name={"colaborador"}
-                type="dropsearch"
-                list={users}
-                multi={true}
-                value={values[2]}
-                placeholder="colaborador..."
-                callback={handleDropdown}
-                searchCall={filterColaborador}
-              />
+          <div className="mt-2 justify-evenly">
+            <div className="flex flex-row w-full gap-4 mb-2">
+              <div className="flex flex-row h-fit justify-between items-center gap-8">
+                <div>
+                  <TextInput
+                    index={1}
+                    name={"tags"}
+                    type="dropsearch"
+                    titleStyle={"font-bold mb-1 text-2xl"}
+                    style={"w-[50rem]"}
+                    placeholder="tags..."
+                    list={tagList}
+                    multi={true}
+                    showTitle={false}
+                    error={"Por favor selecione ou adicione uma tag"}
+                    value={tagValues[1]}
+                    callback={handleDropdownTags}
+                    searchCall={filterTags}
+                  />
+                </div>
+
+                <div>
+                  <TextInput
+                    index={1}
+                    name={"colaborador"}
+                    type="dropsearch"
+                    titleStyle={"font-bold mb-1 text-2xl"}
+                    style={"w-[50rem]"}
+                    placeholder="colaborador..."
+                    list={colabList}
+                    multi={true}
+                    showTitle={false}
+                    error={"Por favor selecione ou adicione um nome"}
+                    value={values[1]}
+                    callback={handleDropdown}
+                    searchCall={filterColab}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -137,8 +184,8 @@ export default function AprovarPublicacao() {
 
       <div className="w-full h-full mt-8 overflow-scroll scrollbar-hide">
         <ul className="justify-start">
-          {publicacoes !== null && publicacoes.length > 0 ? (
-            publicacoes.map((pub, index) => (
+          {Object.keys(filteredList).length > 0 && filteredList !== null ? (
+            filteredList.map((pub, index) => (
               <li key={index}>
                 <div className="pb-4">
                   <div>
@@ -167,3 +214,52 @@ export default function AprovarPublicacao() {
     </div>
   );
 }
+
+/*
+
+  const handleType = (index, event) => {
+    let data = [...values];
+    data[index] = event.target.value;
+    setValues(data);
+  };
+  
+
+          <div className="w-full pb-2">
+            <TextInput
+              index={0}
+              type="searchbar"
+              noTitle={true}
+              callback={handleType}
+              value={values[0]}
+            />
+          </div>
+          
+            <div className="w-full pr-2">
+              <TextInput
+                index={1}
+                name={"tags"}
+                type="dropsearch"
+                list={tagList}
+                multi={true}
+                value={values[1]}
+                placeholder="tags..."
+                callback={handleDropdown}
+                searchCall={filterTags}
+              />
+            </div>
+
+            <div className="w-full pl-2">
+              <TextInput
+                index={2}
+                name={"colaborador"}
+                type="dropsearch"
+                list={users}
+                multi={true}
+                value={values[2]}
+                placeholder="colaborador..."
+                callback={handleDropdown}
+                searchCall={filterColaborador}
+              />
+            </div>
+
+*/
